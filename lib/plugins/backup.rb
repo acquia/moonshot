51803deff3c28125905b21969d6da6ee386cb0ff
Gergely Borsothy-Gaal <gergely.borsothy@cheppers.com>
2016-06-13 11:59:46 +0200

    Cleaning up docs

--- lib/plugins/backup.rb
+++ lib/plugins/backup.rb
@@ -3,8 +3,8 @@ require 'zlib'
 require_relative '../moonshot/creds_helper'
 
 module Moonshot::Plugins # rubocop:disable Documentation
-  # Monkeypatching TarWriter for our
-  # needs to improve clarity
+  # Defining new method for the ease of use in this
+  # particular use-case, and patching to TarWriter
   refine Gem::Package::TarWriter do
     def add(path:, name:, permission: nil)
       rel_path = File.join(path << name)
@@ -37,6 +37,11 @@ module Moonshot
         @target_name ||= '%{app_name}_%{timestamp}_%{user}.tar.gz'
       end
 
+      # Factory method to create preconfigured
+      # Backup plugins. Uploads current template
+      # and parameter files.
+      # @param backup [String] target bucket name
+      # @return [Backup] configured backup object
       def self.to_bucket(bucket:)
         raise ArgumentError if bucket.nil?
         Moonshot::Plugins::Backup.new do |b|
@@ -49,6 +54,9 @@ module Moonshot
         end
       end
 
+      # Main worker method, creates a tarball of the given
+      # files, and uploads to an S3 bucket.
+      # @param resources [Resources] injected Moonshot resources
       def backup(resources) # rubocop:disable Metrics/AbcSize
         raise ArgumentError if resources.nil?
 
@@ -72,7 +80,7 @@ module Moonshot
         end
       end
 
-      # dynamically responding to hooks
+      # Dynamically responding to hooks
       # supplied in the constructor
       def method_missing(method_name, *args, &block)
         @hooks.include?(method_name) ? backup(*args) : super
@@ -87,8 +95,9 @@ module Moonshot
       attr_accessor :app_name,
                     :stack_name
 
-      # returns a tar IO object
-      # from the passed files
+      # Creates an IO stream from the passed files
+      # @param target_files [String] array of filenames
+      # @return tar_stream [IO] IO stream of archived files
       def tar(target_files)
         tar_stream = StringIO.new
         Gem::Package::TarWriter.new(tar_stream) do |writer|
@@ -101,8 +110,9 @@ module Moonshot
         tar_stream
       end
 
-      # returns a tarball IO object
-      # from the passed tar file
+      # Zips up the passed tar stream
+      # @param io_tar [IO] tar stream
+      # @return zip_stream [IO] IO stream of zipped file
       def zip(io_tar)
         zip_stream = StringIO.new
         Zlib::GzipWriter.wrap(zip_stream) do |gz|
@@ -113,6 +123,9 @@ module Moonshot
         zip_stream
       end
 
+      # Uploads an object from the pass
+      # IO stream to the specified bucket
+      # @param io_zip [IO] tar stream
       def upload(io_zip)
         s3_client.put_object(
           acl: 'private',
@@ -122,6 +135,9 @@ module Moonshot
         )
       end
 
+      # Renders string with the specified placeholders
+      # @param io_zip [String] raw string with placeholders
+      # @return [String] rendered string
       def render(placeholder)
         format(
           placeholder,
@@ -132,6 +148,9 @@ module Moonshot
         )
       end
 
+      # Renders string with the specified placeholders
+      # @param file [String] array of raw strings
+      # @return [String] array of rendered strings
       def render_path(file)
         file[:path] =
           file[:path].map do |f|
