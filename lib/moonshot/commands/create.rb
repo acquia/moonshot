d460d53881de03b6bf25e8c5219215e6026581d3
Marcio Toshio Ide <marcio@tosh.io>
2022-10-25 16:55:22 -0300

    CPD-7518: Add tags option to create command. (#287)

--- lib/moonshot/commands/create.rb
+++ lib/moonshot/commands/create.rb
@@ -21,6 +21,19 @@ module Moonshot
         parser.on('--version VERSION_NAME', 'Version for initial deployment. If unset, a new development build is created from the local directory') do |v| # rubocop:disable LineLength
           @version = v
         end
+
+        parser.on('--template-file=FILE', 'Override the path to the CloudFormation template.') do |v|
+          Moonshot.config.template_file = v
+        end
+
+        parser.on('--tag KEY=VALUE', '-TKEY=VALUE', 'Specify stack tags on the command line') do |v|
+          data = v.split('=', 2)
+          unless data.size == 2
+            raise "Invalid tag format '#{v}', expected KEY=VALUE (e.g. MyStackTag=12)"
+          end
+
+          Moonshot.config.extra_tags << { key: data[0], value: data[1] }
+        end
       end
 
       def execute
